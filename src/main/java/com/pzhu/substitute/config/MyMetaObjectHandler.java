8e5ae9ceeda68dc1d58354cfa29169cbea4d5791
github <987472953@qq.com>
2022-02-24 00:37:53 +0800

    refactor(配置文件): mybatis plus 自动插入

---
+++ src/main/java/com/pzhu/substitute/config/MyMetaObjectHandler.java
@@ -0,0 +1,28 @@
+package com.pzhu.substitute.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author dengyiqing
+ * @description MyBatisPlus 自动更新
+ * @date 2022/1/18
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date()); // 起始版本 3.3.0(推荐使用)
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date()); // 起始版本 3.3.0(推荐使用)
+    }
+
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date()); // 起始版本 3.3.0(推荐使用)
+    }
+}
