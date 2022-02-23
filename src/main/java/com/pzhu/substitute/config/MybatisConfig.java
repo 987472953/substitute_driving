c6a8bce8efe20145ab8a04676a6fdaaff5a5af12
github <987472953@qq.com>
2022-02-24 00:37:01 +0800

    refactor(配置文件): 表前缀修改

---
+++ src/main/java/com/pzhu/substitute/config/MybatisConfig.java
@@ -0,0 +1,20 @@
+package com.pzhu.substitute.config;
+
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dengyiqing
+ * @description mbp 的配置类
+ * @date 2022/2/23
+ */
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizer(){
+        return builder -> builder.featuresToEnable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+    }
+}
--- src/main/java/com/pzhu/substitute/entity/UserInfo.java
+++ src/main/java/com/pzhu/substitute/entity/UserInfo.java
@@ -1,8 +1,14 @@
 package com.pzhu.substitute.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
@@ -13,129 +19,47 @@ import java.util.Date;
  * @description (UserInfo)实体类
  * @since 2022-01-11 16:51:27
  */
-@TableName(value = "user_info")
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ApiModel("角色")
 public class UserInfo extends Model<DriverInfo> implements Serializable {
     private static final long serialVersionUID = -54996581787580871L;
     /**
      * ID
      */
     @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty("角色名称")
     private Long id;
-    /**
-     * 微信登录标识
-     */
+
+    @ApiModelProperty("微信登录标识")
     private Integer openid;
-    /**
-     * 手机号
-     */
+
+    @ApiModelProperty("手机号")
     private String phoneNum;
-    /**
-     * 登录密码
-     */
+
+    @ApiModelProperty("登录密码")
     private String password;
-    /**
-     * 昵称
-     */
+
+    @ApiModelProperty("昵称")
     private String nickname;
-    /**
-     * 头像
-     */
+
+    @ApiModelProperty("头像")
     private String avatar;
-    /**
-     * 性别
-     */
+
+    @ApiModelProperty("性别")
     private Integer sex;
+
     @ApiModelProperty(value = "年龄")
     private Integer age;
+
     @ApiModelProperty(value = "创建日期", example = "2020-02-05 13:30:41")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss", iso = DateTimeFormat.ISO.DATE_TIME)
     @TableField(value = "create_time", fill = FieldFill.INSERT)
     private Date createTime;
+
     @ApiModelProperty(value = "更新日期", example = "2020-02-05 13:30:41")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss", iso = DateTimeFormat.ISO.DATE_TIME)
     @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getOpenid() {
-        return openid;
-    }
-
-    public void setOpenid(Integer openid) {
-        this.openid = openid;
-    }
-
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
 }
 
