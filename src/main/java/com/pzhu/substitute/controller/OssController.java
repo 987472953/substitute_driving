044abe50b1599d3865b03e8a36f7894ad414852f
987472953 <987472953@qq.com>
2022-04-13 15:37:32 +0800

    feat(文件上传管理): 添加接口

---
+++ src/main/java/com/pzhu/substitute/controller/OssController.java
@@ -0,0 +1,57 @@
+package com.pzhu.substitute.controller;
+
+import com.pzhu.substitute.common.ConstantPropertiesUtil;
+import com.pzhu.substitute.common.Result;
+import com.pzhu.substitute.service.FileService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author dengyiqing
+ * @description oss文件管理
+ * @date 2022/4/9
+ */
+@RestController
+@Api(tags = "Oss管理")
+@RequestMapping("oss")
+@Slf4j
+public class OssController {
+
+    @Autowired
+    private FileService fileService;
+
+
+    @PostMapping("file/upload")
+    public Result upload(
+            @ApiParam(name = "file", value = "文件", required = true)
+            @RequestParam("file") MultipartFile file,
+
+            @ApiParam(name = "host", value = "文件上传路径", required = false)
+            @RequestParam(value = "host", required = false) String host) {
+
+        if (!StringUtils.isEmpty(host)) {
+            ConstantPropertiesUtil.FILE_HOST = host;
+        } else {
+            ConstantPropertiesUtil.FILE_HOST = "avatar";
+        }
+        String url = fileService.upload(file);
+        return Result.ok().data("url", url);
+    }
+
+//    public String getUrl(String key) {
+//        initClient();
+//        // 设置URL过期时间为1小时。
+//        Date expiration = new Date(new Date().getTime() + 3600 * 1000);
+//        // 生成以GET方法访问的签名URL，访客可以直接通过浏览器访问相关内容。
+//        URL url = client.generatePresignedUrl(bucketName, key, expiration);
+//        return url.toString();
+//    }
+}
